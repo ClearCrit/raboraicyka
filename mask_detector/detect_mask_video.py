c0dd1ffc87eb2f00c7cf62c7bc8f3fac1b16f581
0rk <trypacan@yandex.ru>
2021-03-19 11:09:03 +0700

    change print to logging, added upper case option for exit, add exception, little renaming

--- mask_detector/detect_mask_video.py
+++ mask_detector/detect_mask_video.py
@@ -4,108 +4,122 @@ from tensorflow.keras.models import load_model
 from imutils.video import VideoStream
 import numpy as np
 import imutils
-import time
 import cv2
-import os
+import logging
+import sys
+from datetime import datetime
 
-def detect_and_predict_mask(frame, faceNet, maskNet):
-	# создание блоба по размеру фрейма
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-		(104.0, 177.0, 123.0))
+logging.basicConfig(level=logging.INFO, filename='shape_log_{:%Y_%m_%d}.log'.format(datetime.now()),
+                    format='%(asctime)s %(levelname)s:%(message)s')
+logger = logging.getLogger(__name__)
 
-	# прогонка блоба через нейронку для определения лица
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
-	print(detections.shape)
 
-	# инициализация списка лиц и их месторасположения, + список прогнозов из нашей нейронки
-	faces = []
-	locs = []
-	preds = []
+def shape_log(detections):
+    logger.info("shape {}".format(detections.shape))
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
-	# постоянно крутить обнаружение
-	for i in range(0, detections.shape[2]):
-		# сохранение вероятности обнаружения
-		confidence = detections[0, 0, i, 2]
 
-                # фильтрация кривого обнаружения, для повышения точности confidence 
-		if confidence > 0.5:
-		# вычисляем (x, y)координаты при ограничениях прямоугольника для объекта
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+def detect_and_predict_mask(frame, face_net, mask_net):
+    # создание блоба по размеру фрейма
+    h, w = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+                                 (104.0, 177.0, 123.0))
 
-		# проверка, что ограничивающие рамки не выходят за пределы фрейма
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+    # прогонка блоба через нейронку для определения лица
+    face_net.setInput(blob)
+    detections = face_net.forward()
+    shape_log(detections)
 
-                        #извлекаем ROI(область интереса изображения), конвертируем из BGR to RGB, меняем размер на 224х224 и в обработку
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
+    # инициализация списка лиц и их месторасположения, + список прогнозов из нашей нейронки
+    faces = []
+    coordinates = []
+    probabilities = []
 
-			#добавление граней и рамок ограничений к соотвестющему листу
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
+    # постоянно крутить обнаружение
+    for i in range(0, detections.shape[2]):
+        # сохранение вероятности обнаружения
+        confidence = detections[0, 0, i, 2]
 
-	# делать прогноз, если хоть 1 лицо обнаружено
-	if len(faces) > 0:
-		# чтобы быстрее работало, удобнее делать прогнозы для всех лиц в одно время, чем по очередно
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32)
+        # фильтрация кривого обнаружения, для повышения точности confidence
+        if confidence > 0.5:
+            # вычисляем (x, y)координаты при ограничениях прямоугольника для объекта
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            startX, startY, endX, endY = box.astype("int")
 
-	# возвращение кортежа местоположения лиц и их текущего расположения
-	return (locs, preds)
+            # проверка, что ограничивающие рамки не выходят за пределы фрейма
+            startX, startY = (max(0, startX), max(0, startY))
+            endX, endY = (min(w - 1, endX), min(h - 1, endY))
 
-# подгрузка сериализованной обученной модели и её десериализация
-prototxtPath = r"face_detector\deploy.prototxt"
-weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
-faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+            # извлекаем ROI(область интереса), конвертируем из BGR to RGB, меняем размер на 224х224 и в обработку
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)  # падает
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
 
-# подгрузка модели
-maskNet = load_model("mask_detector.model")
+            # добавление граней и рамок ограничений к соотвестющему листу
+            faces.append(face)
+            coordinates.append((startX, startY, endX, endY))
 
-# инициализация потока видео
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+    # делать прогноз, если хоть 1 лицо обнаружено
+    if len(faces) > 0:
+        # чтобы быстрее работало, удобнее делать прогнозы для всех лиц в одно время, чем по очередно
+        faces = np.array(faces, dtype="float32")
+        probabilities = mask_net.predict(faces, batch_size=32)
 
-# циклю при рабочем потоке видео
-while True:
-	#захват видео с потока и ресайз до максимальных 400 пикселей
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+    # возвращение кортежа местоположения лиц и их текущего расположения
+    return coordinates, probabilities
 
-	# определение лиц на видеопотоке и определение есть ли на них маска
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
-	# перебор всех лиц и их текущего местарасположения на видео
-	for (box, pred) in zip(locs, preds):
-		# распаковка ограничивающей рамке и прогнозов
-		(startX, startY, endX, endY) = box
-		(mask, withoutMask) = pred
+if __name__ == "__main__":
+    # подгрузка сериализованной обученной модели и её десериализация
+    proto_txt_path = r"face_detector\deploy.prototxt"
+    weights_path = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+    face_net = cv2.dnn.readNet(proto_txt_path, weights_path)
 
-		# определение лейбла и цвета для отрисовки ограничевающих рамок
-		label = "In mask" if mask > withoutMask else "Without mask"
-		color = (0, 255, 0) if label == "In mask" else (0, 0, 255)
+    # подгрузка модели
+    mask_net = load_model("mask_detector.model")
 
-		# добавление вероятности в лейбл
-		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+    # инициализация потока видео
+    logger.info("starting video stream...")
+    vs = VideoStream(src=0).start()
 
-		# отображение лейбла и ограничивающих рамок поверх видеопотока
-		cv2.putText(frame, label, (startX, startY - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+    # циклю при рабочем потоке видео
+    while True:
+        # захват видео с потока и ресайз до максимальных 400 пикселей
+        frame = imutils.resize(vs.read(), width=400)
 
-	# вывод
-	cv2.imshow("Video", frame)
-	key = cv2.waitKey(1) & 0xFF
+        # определение лиц на видеопотоке и определение есть ли на них маска
+        try:
+            locs, preds = detect_and_predict_mask(frame, face_net, mask_net)  # падает
+            for box, pred in zip(locs, preds):
+                # распаковка ограничивающей рамке и прогнозов
+                startX, startY, endX, endY = box
+                mask, withoutMask = pred
 
-	# остановить видео поток, если нажать q
-	if key == ord("q"):
-		break
+                # определение лейбла и цвета для отрисовки ограничевающих рамок
+                label, color = ("In mask", (0, 255, 0)) if mask > withoutMask else ("Without mask", (0, 0, 255))
 
-# отчистка 
-cv2.destroyAllWindows()
-vs.stop()
+                # добавление вероятности в лейбл
+                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+                # отображение лейбла и ограничивающих рамок поверх видеопотока
+                cv2.putText(frame, label, (startX, startY - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+            # вывод
+            cv2.imshow("Video", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            # остановить видео поток, если нажать q
+            if key in (ord("q"), ord("Q")):
+                break
+        except Exception as e:
+            logger.error(e)
+            logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+        # перебор всех лиц и их текущего местарасположения на видео
+
+    # отчистка
+    cv2.destroyAllWindows()
+    vs.stop()
